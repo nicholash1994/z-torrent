ab000762c1625f41ed4ca60ec64b9900a82e18d1
Nick <nicholashannani@yahoo.com>
2018-02-18 22:18:10 -0500

    Changes to be committed: 	modified:   main.c

--- main.c
+++ main.c
@@ -3,12 +3,45 @@
 #include <stdio.h>
 #include "unicode.h"
 
-int main() {
-	uchar* unicode_string;
-	uchar uc;
-	int i = 0;
+void scan_unicode_string();
 
-	unicode_string = (uchar*)malloc(80*sizeof(uchar));
+struct torrent {
+
+};
+
+union bval {
+	uchar* val;
+	struct bdict* dict;
+};
+
+struct bdict {
+	uchar* key;
+	union bval val;
+	struct bdict* next;
+};
+
+
+struct bdict read_torrent_file(const char* filename);
+
+int main() {
+	FILE* torrent_file;
+	uchar uc;
+	int i;
+	unsigned int n;
+
+	torrent_file = fopen("./ubuntu.torrent", "rb");
+	printf("File opened successfully!\n");
+
+
+	fclose(torrent_file);
+
+	return 0;
+} 
+
+void scan_unicode_string() {
+	uchar unicode_string[80];
+	uchar uc;
+	int i;
 
 	// Prompt user for unicode string
 	printf("Enter unicode string: ");
@@ -18,6 +51,36 @@ int main() {
 	
 	printf("Byte length of unicode string: %lu\n", strlen(unicode_string));	
 	printf("Character length of unicode string: %u\n", ustrlen(unicode_string));
+}
 
-	return 0;
-} 
+/*  
+struct bdict read_torrent_file(const char* filename) {
+	FILE* torrent;
+	uchar uc;
+	uchar key_name[30];
+	int done_reading_file;
+
+	torrent = fopen(filename, "rb");
+	if (torrent = NULL) {
+		fprintf(stderr, "Error: torrent file couldn't be read!\n");
+		exit(-1);
+	}
+
+	done_reading_file = 0;	
+	while (!done_reading_file) {
+		read_key(torrent, key_name);
+		strncmp(key_name, "announce", 30);
+	}
+}
+*/
+
+void read_key(FILE* file, uchar* dest) {
+	int i;
+	uchar uc;
+	
+	i = 0;
+	while ((uc=fgetc(file)) != ':' && i < 30)
+		dest[i++] = uc;
+	dest[i] = '\0';
+}
+
