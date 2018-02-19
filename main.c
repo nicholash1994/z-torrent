82421d597fbeed7865bf71c3b3bf3a7e6578430f
Nick <nicholashannani@yahoo.com>
2018-02-18 19:53:54 -0500

    Changes to be committed: 	modified:   main.c

--- main.c
+++ main.c
@@ -9,12 +9,15 @@ int main() {
 	int i = 0;
 
 	unicode_string = (uchar*)malloc(80*sizeof(uchar));
+
+	// Prompt user for unicode string
+	printf("Enter unicode string: ");
 	while ((uc = getchar()) != '\n' && i < 80)
 		unicode_string[i++] = uc;
 	unicode_string[i] = '\0';
 	
-	printf("Byte length of unicode string: %d\n", strlen(unicode_string));	
-	printf("Character length of unicode string: %d\n", ustrlen(unicode_string));
+	printf("Byte length of unicode string: %lu\n", strlen(unicode_string));	
+	printf("Character length of unicode string: %u\n", ustrlen(unicode_string));
 
 	return 0;
 } 
